6185bdb6615f84bc946161bf1e3ba3b7f1bba866
GallasMahdi <168534833+GallasMahdi@users.noreply.github.com>
2026-05-09 15:01:08 +0100

    feat: add constants for event details, theme colors, program schedule, and navigation links

--- wafa-connect/src/lib/constants.ts
+++ wafa-connect/src/lib/constants.ts
@@ -7,10 +7,10 @@ export const EVENT = {
   date: new Date('2026-05-21T18:00:00+01:00'), // Africa/Tunis UTC+1
   dateLabel: 'Jeudi 21 Mai 2026',
   timeLabel: 'À partir de 18h00',
-  venue: 'Centre Urbain Nord',
-  city: 'Tunis, Tunisie',
-  mapsUrl: 'https://www.google.com/maps/search/Centre+Urbain+Nord+Tunis',
-  mapsEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3193.0!2d10.186!3d36.832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzbCsDQ5JzU1LjIiTiAxMMKwMTEnMDkuNiJF!5e0!3m2!1sen!2stn!4v1',
+  venue: 'Attijari Assurance Tunisie',
+  city: 'Lot N°A14, Bd de la Terre, Tunis 1082',
+  mapsUrl: 'https://maps.app.goo.gl/jqb9Wgpuz9br6A8x8?g_st=iw',
+  mapsEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3191.0336214041!2d10.1974974766861!3d36.84069646545161!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd353d14cacb23%3A0xb93be5026d212842!2sAttijari%20Assurance%20Tunisie!5e0!3m2!1sfr!2stn!4v1715263445781!5m2!1sfr!2stn',
 } as const
 
 export const COLORS = {
